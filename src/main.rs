593e9e525e130df3969a3d94f1a5dcbc6761bdde
Taiga Ishida <tonythetaiga@gmail.com>
2025-05-03 19:24:52 -0400

    scanning, file type detection

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,49 @@
-use notify::{Event, RecursiveMode, Result, Watcher, recommended_watcher};
+use notify::{RecursiveMode, Watcher, recommended_watcher};
 use std::path::Path;
+use tokio::fs;
 use tokio::sync::mpsc;
 
-fn handle_event(event: Event) {
-    println!("GOT = {:?}", event);
+fn get_file_type(path_str: &str) -> std::result::Result<&'static str, Box<dyn std::error::Error>> {
+    match infer::get_from_path(path_str) {
+        Ok(Some(kind)) => Ok(kind.mime_type()),
+        Ok(None) => Ok("text/plain"), // unreliable
+        Err(_) => Err("failed to get file type".into()),
+    }
+}
+
+async fn handle_event(event: notify::Event) -> std::result::Result<(), Box<dyn std::error::Error>> {
+    println!("event: {:?}", event);
+    for path in event.paths {
+        // Use async metadata to avoid blocking
+        if let Ok(meta) = fs::metadata(&path).await {
+            if meta.is_file() {
+                if let Some(path_str) = path.to_str() {
+                    let file_type = get_file_type(path_str)?;
+                    println!("detected file type: {:?}", file_type);
+                } else {
+                    return Err("Failed to handle event".into());
+                }
+            }
+        }
+    }
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() {
-    let (tx, mut rx) = mpsc::channel::<Result<Event>>(32);
+    let (tx, mut rx) = mpsc::channel::<notify::Result<notify::Event>>(32);
     let tx_clone = tx.clone();
     tokio::task::spawn_blocking(move || {
-        let watcher_init = recommended_watcher(move |res: Result<Event>| {
+        let watcher_init = recommended_watcher(move |res: notify::Result<notify::Event>| {
             let _ = tx_clone.blocking_send(res);
         });
 
         if let Ok(mut watcher) = watcher_init {
             watcher
-                .watch(Path::new("./data"), RecursiveMode::Recursive)
+                .watch(
+                    Path::new("/Users/taiga/Code/data"),
+                    RecursiveMode::Recursive,
+                )
                 .unwrap();
             std::thread::park();
         } else {
@@ -26,8 +52,11 @@ async fn main() {
     });
 
     while let Some(result) = rx.recv().await {
-        if let Ok(event) = result {
-            handle_event(event);
+        match result {
+            Ok(event) => {
+                let _ = handle_event(event).await;
+            }
+            Err(error) => println!("Failed to initialize watcher! {:?}", error),
         }
     }
 }
